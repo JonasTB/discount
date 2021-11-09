a8a74e7efdbfa13e24b4c9e2f8b11f5c313c156e
JonasTB <jonastimbauba7@hotmail.com>
2021-11-09 17:04:58 -0300

    fixed route auth.

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,6 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/course', require('./courseRoutes'));
-router.use('/user', require('./authRoutes'));
+router.use('/auth', require('./authRoutes'));
 
 module.exports = router;
